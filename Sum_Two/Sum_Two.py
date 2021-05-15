f62983d3d4e739e81a9dbeb0ebd160fc44163ad7
VbaGGz <vinnyippo@gmail.com>
2021-05-15 13:29:16 -0400

    Sum_Two

---
+++ Sum_Two/Sum_Two.py
@@ -0,0 +1,14 @@
+
+nums = [2,7,11,15]
+target = 9
+
+def Sum_Two(nums,target):
+    numDict={}
+    n=len(nums)
+    for i in range(0,n):
+        sub=target-nums[i]
+        if(sub in numDict):
+            return [numDict[sub], i]
+        numDict[nums[i]]=i
+
+print(Sum_Two(nums,target))
